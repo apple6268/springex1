5b1eb538b5cdf4796aa743bda3f86c14125a35ed
admin <whwo213@naver.com>
2015-02-10 22:49:55 +0900

    deploy

---
+++ src/main/java/com/example/controller/PersonController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import java.util.Map;
+
+@Controller
+public class PersonController {
+    @Autowired
+    private SqlSessionTemplate sessionTemplate;
+
+    @RequestMapping("/")
+    public String listPeople(Map<String, Object> map) {
+        this.sessionTemplate.selectOne("users.test");
+        return "main";
+    }
+
+
+}
